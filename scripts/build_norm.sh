90f76d440552f45a7293111c22ff72572c7b1bb8
Mate Soos <soos.mate@gmail.com>
2026-03-08 20:40:09 +0100

    Update scripts and nix

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles lib main Makefile .cmake
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-make -j$(nproc)
+make "-j$(nproc)"
--- scripts/build_static_release.sh
+++ scripts/build_static_release.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles lib main Makefile .cmake
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DSTATICCOMPILE=ON ..
 make -j$(nproc)
