842ea1053f7de65a1919e74cb9a9dd464f5a8f86
Mate Soos <soos.mate@gmail.com>
2026-04-03 16:37:02 +0200

    Fix build scripts

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles lib main Makefile .cmake
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSTATICCOMPILE=ON ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=OFF ..
 make -j$(nproc)
--- scripts/build_static_release.sh
+++ scripts/build_static_release.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles lib main Makefile .cmake
-cmake -DCMAKE_BUILD_TYPE=Release -DSTATICCOMPILE=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF ..
 make -j$(nproc)
