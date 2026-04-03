ea2d0a8321b88931face9e5d75391a48c7dd5cca
Mate Soos <soos.mate@gmail.com>
2026-04-03 16:39:30 +0200

    Fix bulid scripts

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles lib main Makefile .cmake
+rm -rf cmake_install.cmake
+rm -rf CMakeCache.txt
+rm -rf CMakeFiles
+rm -rf lib
+rm -rf main
+rm -rf Makefile
+rm -rf .cmake
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -j$(nproc)
--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles lib main Makefile .cmake
+rm -rf cmake_install.cmake
+rm -rf CMakeCache.txt
+rm -rf CMakeFiles
+rm -rf lib
+rm -rf main
+rm -rf Makefile
+rm -rf .cmake
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j$(nproc)
