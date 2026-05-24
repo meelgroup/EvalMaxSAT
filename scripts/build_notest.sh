9587d6e215054bfc74d0c496c01a12afd7a92a6e
Mate Soos <soos.mate@gmail.com>
2026-05-24 10:38:52 +0200

    Add notesting build

---
+++ scripts/build_notest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -rf cmake_install.cmake
+rm -rf CMakeCache.txt
+rm -rf CMakeFiles
+rm -rf lib
+rm -rf main
+rm -rf Makefile
+rm -rf .cmake
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_TESTING=OFF ..
+make "-j$(nproc)"
